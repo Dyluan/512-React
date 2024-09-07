75a42fe28318cf19d65e04ac11238fc827fbf5c7
Dyluan <dylan.devoir@gmail.com>
2024-09-07 23:25:35 +0200

    a bit better innit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,96 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [board, setBoard] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [board, setBoard] = useState(Array(9).fill(2));
+
+  function isOpposes (liste) {
+    //x-0-x 
+    if (liste[0] == liste[2] && liste[1] == 0 && liste[0] != 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+  }
+  function isPremiers (liste) {
+    if (liste[0] == liste[1] && liste[0] != 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+  }
+  function isDerniers (liste) {
+    //y-x-x
+    if (liste[1] == liste[2] && liste[1] != 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+  }
+  function addition(liste) {
+    if (isOpposes(liste)) {
+      // console.log('opposés : ', liste)
+      liste[2] *= 2;
+      liste[1] = 0;
+      liste[0] = 0;
+      
+      return liste;
+    }
+    //fonctionne
+    if (isDerniers(liste)) {
+      // console.log('derniers', liste);
+      liste[2] *= 2;
+      liste[1] = liste[0];
+      liste[0] = 0;
+      
+      return liste;
+    }
+    //fonctionne
+    if(isPremiers(liste)) {
+      // console.log('premiers : ', liste)
+      if (liste[2] == 0) {
+          liste[2] = liste[1]*2;
+          liste[1] = 0;
+          liste[0] = 0;
+      }
+      else {
+          liste[1] *= 2;
+          liste[0] = 0;
+      }
+    }
+    //si aucun doublons
+    else {
+      // 2-4-0 => 0-2-4
+      if (liste[2] == 0 && liste[0]!=0 && liste[1]!=0) {
+          liste[2] = liste[1];
+          liste[1] = liste[0];
+          liste[0] = 0;
+      }
+      // 2-0-4 => 0-2-4
+      else if (liste[1] == 0 && liste[0] != 0 && liste[2] != 0) {
+          liste[1] = liste[0];
+          liste[0] = 0;
+      }
+      //ici on teste les cas de figure où on a 2*0 sur la mm ligne
+      else {
+          //0-2-0
+          if (liste[1]!=0 && liste[0]==0 && liste[2]==0) {
+              liste[2] = liste[1];
+              liste[1] = 0;
+              liste[0] = 0;
+          }
+          //2-0-0
+          else if(liste[0]!=0 && liste[1]==0 && liste[2]==0) {
+              liste[2]=liste[0];
+              liste[1]=0;
+              liste[0]=0;
+          }
+      }
+    }
+  return liste;
+  }
 
   const moveBoard = (e) => {
     switch(e.key) {
@@ -13,13 +102,22 @@ function App() {
           let secondElem = board[i-1];
           let thirdElem = board[i-2];
           
-          let arr = [parseInt(firstElem.innerText), parseInt(secondElem.innerText), parseInt(thirdElem.innerText)];
-          //let shifted = addition(arr);
+          let arr = [parseInt(firstElem), parseInt(secondElem), parseInt(thirdElem)];
+          let shifted = addition(arr);
 
-          firstElem += 3;
-          secondElem += 5;
-          thirdElem += 2;
-          console.log('clickos')
+          // firstElem = shifted[0];
+          // secondElem = shifted[1];
+          // thirdElem = shifted[2];
+          
+          setBoard(prevBoard => {
+            const newBoard = [...prevBoard];
+            newBoard[i] = shifted[0];
+            newBoard[i-1] = shifted[1];
+            newBoard[i-2] = shifted[2];
+            return newBoard;
+          });
+
+          console.log(board)
       }
 
       break;
