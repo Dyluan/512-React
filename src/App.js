7eb267742e26a1f6ffaebd6139ef1b1ad99c8b4f
Dyluan <dylan.devoir@gmail.com>
2024-09-07 22:46:35 +0200

    created the base environnement for the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [board, setBoard] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const moveBoard = (e) => {
+    switch(e.key) {
+      case 'ArrowLeft' :
+        for (let i=board.length-1; i>0; i-=3) {
+          let firstElem = board[i];
+          let secondElem = board[i-1];
+          let thirdElem = board[i-2];
+          
+          let arr = [parseInt(firstElem.innerText), parseInt(secondElem.innerText), parseInt(thirdElem.innerText)];
+          //let shifted = addition(arr);
+
+          firstElem += 3;
+          secondElem += 5;
+          thirdElem += 2;
+          console.log('clickos')
+      }
+
+      break;
+    }
+  }
+
+  const bodyListener = () => {
+    document.body.addEventListener('keydown', (e) => {
+      e.preventDefault();
+      console.log('jappuie ', e);
+      moveBoard(e);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="tictactoe flex flex-col justify-center items-center">
+        <h1><button onClick={bodyListener}>JOUER</button></h1>
+        <div class="board grid grid-cols-3 grid-cols-3 w-28 h-28">
+            <div class="cell border-black border-solid border flex justify-center items-center text-3xl">0</div>
+            <div class="cell border-black border-solid border flex justify-center items-center text-3xl">0</div>
+            <div class="cell border-black border-solid border flex justify-center items-center text-3xl">0</div>
+            <div class="cell border-black border-solid border flex justify-center items-center text-3xl">0</div>
+            <div class="cell border-black border-solid border flex justify-center items-center text-3xl">0</div>
+            <div class="cell border-black border-solid border flex justify-center items-center text-3xl">0</div>
+            <div class="cell border-black border-solid border flex justify-center items-center text-3xl">0</div>
+            <div class="cell border-black border-solid border flex justify-center items-center text-3xl">0</div>
+            <div class="cell border-black border-solid border flex justify-center items-center text-3xl">0</div>
+        </div>
+        <p class="message">Score : </p>
+        <button class="restart">Restart</button>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
